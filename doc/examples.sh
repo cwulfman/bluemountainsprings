64c2c970c4ee254c47bbed425c931e6f9d0cadca
Clifford Wulfman <cwulfman@loki.home>
2016-12-17 13:38:33 -0500

    Adds shell script that runs all the examples in the api doc.

---
+++ doc/examples.sh
@@ -0,0 +1,20 @@
+curl -H'Accept: application/json' http://bluemountain.princeton.edu/exist/restxq/springs/magazines
+curl -H'Accept: application/json' http://bluemountain.princeton.edu/exist/restxq/springs/magazines/bmtnaap
+curl -H'Accept:application/json' http://bluemountain.princeton.edu/exist/restxq/springs/issues/bmtnaap_1921-11_01
+curl -H'Accept:application/tei+xml' http://bluemountain.princeton.edu/exist/restxq/springs/issues/bmtnaap_1921-11_01
+curl -H'Accept:text/plain' http://bluemountain.princeton.edu/exist/restxq/springs/issues/bmtnaap_1921-11_01
+curl -H'Accept:application/rdf+xml' http://bluemountain.princeton.edu/exist/restxq/springs/issues/bmtnaap_1921-11_01
+curl -H'Accept:application/json' http://bluemountain.princeton.edu/exist/restxq/springs/issues/bmtnaap
+curl -H'Accept:application/tei+xml' http://bluemountain.princeton.edu/exist/restxq/springs/issues/bmtnaap
+curl -H'Accept:text/plain' http://bluemountain.princeton.edu/exist/restxq/springs/issues/bmtnaap
+curl -H'Accept:application/json' http://bluemountain.princeton.edu/exist/restxq/springs/constituents/bmtnaap_1921-11_01
+curl -H'Accept:application/json' http://bluemountain.princeton.edu/exist/restxq/springs/constituents/bmtnaap
+curl -H'Accept:text/plain' http://bluemountain.princeton.edu/exist/restxq/springs/constituent/bmtnaap_1921-11_01/c003
+curl -H'Accept:application/tei+xml' http://bluemountain.princeton.edu/exist/restxq/springs/constituent/bmtnaap_1921-11_01/c003
+curl -H'Accept:text/csv' http://bluemountain.princeton.edu/exist/restxq/springs/contributors/bmtnaap_1921-11_01
+curl -H'Accept:application/json' http://bluemountain.princeton.edu/exist/restxq/springs/contributors/bmtnaap_1921-11_01
+curl -H'Accept:application/json' http://bluemountain.princeton.edu/exist/restxq/springs/contributions?byline=Tzara
+curl -H'Accept:application/tei+xml' http://bluemountain.princeton.edu/exist/restxq/springs/contributions?byline=Tzara
+curl http://bluemountain.princeton.edu/exist/restxq/iiif/collection/top
+curl http://bluemountain.princeton.edu/exist/restxq/iiif/collection/bmtnaap
+curl http://bluemountain.princeton.edu/exist/restxq/iiif/manifest/bmtnaap_1921-11_01
